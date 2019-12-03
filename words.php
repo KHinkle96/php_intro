21cc933141ee68301d5e38a6a7d8677510db2b43
KHinkle96 <57928396+KHinkle96@users.noreply.github.com>
2019-12-02 20:53:20 -0800

    Update words.php

--- words.php
+++ words.php
@@ -2,4 +2,18 @@
 echo "Enter a word: ";
 $input = rtrim(fgets(STDIN));
 $wordlength = strlen($input);
-echo "Your word is {$wordlength} letters long";
+echo "Your word is {$wordlength} letters long\n";
+
+function checkWord($word) {
+$len = strlen($word);
+if ($len > 5) {
+return strrev(substr($word, 0, 4));
+} else {
+    return $word;
+}
+}
+
+$result = checkWord($input);
+echo $result;
+
+?>
